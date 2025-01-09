c6df6cdded1b2deff5e7ebd8e4e3d7d9f148b8f0
Denis Angell <dangell@transia.co>
2025-01-09 10:10:12 -0500

    [fold] add batch comment

--- src/xrpld/app/tx/detail/Batch.cpp
+++ src/xrpld/app/tx/detail/Batch.cpp
@@ -258,6 +258,7 @@ Batch::preflight(PreflightContext const& ctx)
 TER
 Batch::doApply()
 {
+    // Inner txns are applied in `applyBatchTransactions`, after the outer batch txn is applied
     return tesSUCCESS;
 }
 
