ca7f105ec99bb95f774733d6d2ec485820114412
Mayukha Vadari <mvadari@gmail.com>
2026-03-22 22:42:48 -0700

    WrappedSLEBase -> SLEBase

--- include/xrpl/ledger/entries/AccountRootHelpers.h
+++ include/xrpl/ledger/entries/AccountRootHelpers.h
@@ -5,7 +5,7 @@
 #include <xrpl/beast/utility/instrumentation.h>
 #include <xrpl/ledger/ApplyView.h>
 #include <xrpl/ledger/ReadView.h>
-#include <xrpl/ledger/entries/WrappedSLEBase.h>
+#include <xrpl/ledger/entries/SLEBase.h>
 #include <xrpl/protocol/Indexes.h>
 #include <xrpl/protocol/Rate.h>
 #include <xrpl/protocol/STLedgerEntry.h>
